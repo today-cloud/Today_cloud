20cc92c0d5f7ee79ebd4f42528cb9a6c5ab34c23
Min-d-h <mdongh89@gmail.com>
2021-11-23 13:12:57 +0900

    21.11.23 file update
    react node 연동

--- app.js
+++ app.js
@@ -2,14 +2,35 @@
 // node.js 를 위한 빠르고 개방적인 웹 프레임 워크
 
 const express = require( 'express' );
+const path = require( 'path' );
 const app = express();
-const port = 8000;
-const body = require( 'body-parser' );
 
+// const port = 8000;
+// const body = require( 'body-parser' );
 
-app.set( 'view engine', 'ejs'); 
 // view의 확장인 ejs 를 사용하도록 도와주는 장치
-app.set( 'views', __dirname + '/views');
+// app.set( 'view engine', 'ejs'); 
+// app.set( 'views', __dirname + '/views');
+
+const http = require('http').createServer(app);
+http.listen(8000, function(){
+    console.log('listening on 8000')
+});
+// app.use( express.static( path.join(__dirname, 'views') ) )
+app.use( '/', express.static(path.join(__dirname, 'views') ) )
+app.use( '/react', express.static(path.join(__dirname, 'react_today/build') ) )
+
+app.get( '/react', ( req, res ) => {
+    // res.sendFile( path.join(__dirname, 'views/main.html') )
+    res.sendFile( path.join(__dirname, 'views/main.html') )
+});
+
+app.get( '/', ( req, res ) => {
+    // res.sendFile( path.join(__dirname, 'views/main.html') )
+    res.sendFile( path.join(__dirname, 'react_today/build/index.html') )
+});
+
+
 
 // ################################ 회원가입 #################################
 app.get( '/signup', ( req, res ) => {
@@ -29,12 +50,31 @@ app.get( '/', ( req, res ) => {
 });
 
 
-app.get( '/', ( req, res ) => {
-    res.send("success");
-});
+// app.get( '/', ( req, res ) => {
+//     res.send("success");
+// });
 
-app.listen( port, () => {
-    console.log('수정 완료');
-});
+// app.listen( port, () => {
+//     console.log('수정 완료');
+// });
 
-// 여기는 app.js입니다.
+// ########################################
+// 아래 방식을 이용해서 파일을 불러오는 방식으로 운영됨.
+// ########################################
+// app.use는 미들웨어 (요청과 응답 사이에 존재하고 실행함.)
+
+// app.use( express.static(path.join(__dirname, 'views') ) )
+
+// const http = require('http').createServer(app);
+// http.listen(8000, function(){
+//     console.log('listening on 8000')
+// });
+
+// app.get( '/', ( req, res ) => {
+//     res.sendFile( path.join(__dirname, 'views/main.html') )
+// });
+
+// * == 사용자가 아무 주소나 입력했을 때 해당 연결된 곳으로 자동연결되게 만듬. ==> 이것이 리액트 라우터
+// app.get( '*', ( req, res ) => {
+//     res.sendFile( path.join(__dirname, 'views/main.html') )
+// });
---
+++ react_today/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(){
+    return(
+        <div>
+            <h1>ssac 3기</h1>
+            <h2>react 수업</h2>
+        </div>
+    )
+}
+export default App;
