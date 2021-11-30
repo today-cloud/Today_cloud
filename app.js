032760e51ce5ae2a4e5b3a4586ffd6f9fa2c4c45
Min-d-h <mdongh89@gmail.com>
2021-11-30 13:33:51 +0900

    21.11.30 - node update
    node update

--- app.js
+++ app.js
@@ -26,30 +26,29 @@ const http = require('http').createServer(app);
 http.listen(8000, function(){
     console.log('listening on 8000')
 });
-// app.use( express.static( path.join(__dirname, 'views') ) )
 app.use( '/', express.static(path.join(__dirname, 'views') ) )
-app.use( '/react', express.static(path.join(__dirname, 'react_today/build') ) )
+app.use( express.static(path.join(__dirname, 'static') ) )
+// ##################react 불러오는 곳#########################
+// app.use( '/react', express.static(path.join(__dirname, 'react_today/build') ) )
 
-app.get( '/react', ( req, res ) => {
+// ##################react 불러오는 곳#########################
+// app.get( '/react', ( req, res ) => {
     // res.sendFile( path.join(__dirname, 'views/main.html') )
-    res.sendFile( path.join(__dirname, 'views/main.html') )
-});
-
-app.get( '/', ( req, res ) => {
-    console.log(conn);
     // res.sendFile( path.join(__dirname, 'views/main.html') )
-    res.sendFile( path.join(__dirname, 'react_today/build/index.html') )
-    // res.render('mainTest');
-});
+// });
 
 // 회원가입 테스트 했습니다.-윤영우-
+app.get( '/', ( req, res ) => {
+    console.log(conn);
+    res.render('mainTest');
+    // ##################react 불러오는 곳#########################
+    // res.sendFile( path.join(__dirname, 'react_today/build/index.html') )
+});
 app.post('/mainTest',(req,res)=>{
-
     res.send('일단 전송됨');
     console.log(req.body);
     var sql = "INSERT INTO T_User (user_Eid, user_Pw, user_Name) values('"+ req.body.eid +"','" +req.body.password +"','"+req.body.name+"');";
     // insert into T_User(user_Eid, user_Pw, user_Name) values('aaa@naver.com','1234','홍길동');
-
     conn.query(sql, function(err, result) {
         if( err ){
 			console.log( 'failed!! : ' + err );
@@ -58,9 +57,12 @@ app.post('/mainTest',(req,res)=>{
 			console.log( "data inserted!" );
 		}
     });
-
 });
 
+// ################################ index확인하는 곳 #################################
+app.get( '/indexAll', ( req, res ) => {
+    res.render('indexAll');
+});
 // ################################ 회원가입 #################################
 app.get( '/signup', ( req, res ) => {
     res.render('signup');
@@ -70,14 +72,17 @@ app.get( '/login', ( req, res ) => {
     res.render('login');
 });
 // ################################ 회원 정보 수정 #################################
-app.get( '/delete', ( req, res ) => {
-    res.render('delete');
+app.get( '/update', ( req, res ) => {
+    res.render('update');
 });
 // ################################ 회원탈퇴 #################################
-app.get( '/', ( req, res ) => {
-    res.send("success");
+app.get( '/delete', ( req, res ) => {
+    res.send("delete");
 });
-
+// ################################ gps확인하는 곳 #################################
+app.get( '/gps', ( req, res ) => {
+    res.render('gps');
+});
 
 // app.get( '/', ( req, res ) => {
 //     res.send("success");
