9590961918f8434413570156cf9dea2b9a776ca8
YOON-YOUNGWOO <yyw1231@naver.com>
2021-12-05 14:26:34 +0900

    sign up
    회원가입 기능

--- app.js
+++ app.js
@@ -2,24 +2,24 @@
 const express = require( 'express' );
 const path = require( 'path' );
 const app = express();
-// const mysql = require( 'mysql' );
-// const session = require('express-session');
-// const body = require( 'body-parser' );
+const mysql = require( 'mysql' );
+const session = require('express-session');
+const body = require( 'body-parser' );
 
 // const userRoute = require('./routes/user')(app);
 // app.use('/', userRoute);
 
-// var conn = mysql.createConnection({
-//   user: 'root',
-//   password: '1234',
-//   database: 'cloud'
-// });
+var conn = mysql.createConnection({
+  user: 'root',
+  password: '1234',
+  database: 'cloud'
+});
 
-// app.use(session({
-//   secret : 'customer',
-//   resave : false,
-//   saveUninitialized : true,
-// }));
+app.use(session({
+  secret : 'customer',
+  resave : false,
+  saveUninitialized : true,
+}));
 
 // const port = 8000;
 // const fs = require('fs');
@@ -30,8 +30,8 @@ const http = require("http");
 app.set( 'view engine', 'ejs');
 app.set( 'views', __dirname + '/views');
 
-// app.use(body.urlencoded({extends:true}));
-// app.use(body.json());
+app.use(body.urlencoded({extends:false}));
+app.use(body.json());
 
 // const options = { // letsencrypt로 받은 인증서 경로를 입력
 //   ca: fs.readFileSync('/etc/letsencrypt/live/todaycloud.shop/fullchain.pem'),
@@ -57,10 +57,32 @@ app.use( '/', express.static(path.join(__dirname, 'views') ) )
 // res.sendFile( path.join(__dirname, 'views/main.html') )
 // });
 
-// 회원가입 테스트 했습니다.-윤영우-
 app.get( '/', ( req, res ) => {
   // console.log(conn);
   res.render('mainTest');
   // ##################react 불러오는 곳#########################
   // res.sendFile( path.join(__dirname, 'react_today/build/index.html') )
+});
+
+// 회원가입 테스트 했습니다.-윤영우 12/05-
+app.get('/signup',(req,res)=>{
+  console.log('회워가입 요청');
+  res.render('signup');
+});
+app.post('/signup',(req,res)=>{
+  var eid = req.body.user_Eid;
+  var pw = req.body.user_Pw;
+  var name = req.body.user_Name;
+  console.log(eid);
+  console.log(pw);
+  console.log(name);
+  const sql = "insert into T_User (user_Eid,user_Pw,user_Name) values ('"+req.body.user_Eid+"','"+req.body.user_Pw+"','"+req.body.user_Name+"');";
+  conn.query(sql,function(err,result){
+    if(err){
+      console.log(err);
+    }else{
+      console.log('insert success');
+      res.render('login');
+    }
+  });
 });
