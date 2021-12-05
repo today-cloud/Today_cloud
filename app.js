da4f4e16dfde86606cf7fd50cc57947519f49bc3
YOON-YOUNGWOO <yyw1231@naver.com>
2021-12-05 16:36:14 +0900

    update delete
    update delete

--- app.js
+++ app.js
@@ -91,7 +91,6 @@ app.post('/signup',(req,res)=>{
 app.get('/login',(req,res)=>{
   res.render('login');
 });
-const querystring = require('querystring');
 app.post('/login',(req,res)=>{
   const eid = req.body.user_Eid;
   const password = req.body.user_Pw;
@@ -99,7 +98,7 @@ app.post('/login',(req,res)=>{
   const sql = "select * from T_User where user_Eid = '" + eid +"' and user_Pw ='" +password+"';";
   conn.query(sql, function(err,result){
     if(result.length == 0) {
-      res.render('/login');
+      res.render('login');
     }else{
       req.session.uid = result[0].user_Num;
       req.session.save(function(err){
@@ -118,3 +117,62 @@ app.get('/logout',(req,res)=>{
     res.redirect('login');
   }
 })
+
+// update id,pw 변경 -윤영우 12/05-
+app.post('/update',(req,res)=>{
+  var eid = req.body.user_Eid;
+  var pw = req.body.user_Pw;
+
+  var sql = "update T_User set user_Eid = '"+eid+"', user_Pw = '"+pw+"' where user_Num = "+req.session.uid+"";
+  const user_sql = "select * from T_User where user_Eid = '" + eid +"'";
+  const sql2 = "select * from T_User where user_Num = '" + req.session.uid +"'";
+  conn.query(user_sql,function(err,results){
+    if(results[0]){
+      res.send(
+          `<script>
+            alert('id 중복!');
+            location.href='${'../main'}';
+          </script>`
+      );
+    }else{
+      conn.query(sql,function(err,result){
+        console.log(result.affectedRows);
+        if(result.affectedRows != 1){
+          res.render('main');
+        }else{
+          conn.query(sql2,function(err,result){
+            console.log('update success');
+            res.render('main',{user:result[0]});
+          });
+        }
+      });
+    }
+  });
+});
+
+//main
+app.get('/main',(req,res)=>{
+  var sql = "select * from T_User where user_Num = '" + req.session.uid +"'";
+  conn.query(sql,function(err,result){
+    if(result.length == 0) {
+      res.render('login');
+    }else{
+      req.session.uid = result[0].user_Num;
+      req.session.save(function(err){
+        res.render('main',{user:result[0]});
+      });
+    }
+  });
+});
+
+// 탈퇴 -윤영우 12/05-
+app.get('/delete',(req,res)=>{
+  var sql = "delete from T_User where user_Num = "+req.session.uid;
+  conn.query(sql,function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect('../login');
+    }
+  });
+});
