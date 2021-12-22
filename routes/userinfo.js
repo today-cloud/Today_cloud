1ce27e7dd39cbc2c5ea149e36da975feefe1a29d
hansm <hanmix2003@naver.com>
2021-12-22 16:53:13 +0900

    userinfo 작업
    userinfo ejs, js 작업

--- routes/userinfo.js
+++ routes/userinfo.js
@@ -8,10 +8,27 @@ var conn = mysql.createConnection({
   database: 'cloud'
 });
 
+function user_sql(req,callback){
+  const sql = "select * from T_User where user_Num = '" + req.session.uid +"';";
+  conn.query(sql, function(err,result){
+    if(result.length == 0) {
+      res.redirect('/login');
+    }else{
+      var user = result[0];
+      console.log("user 정보 ")
+      console.log(user);
+      callback(user);
+    }
+  });
+}
 
 router.get('/userinfo',(req,res)=>{
   console.log('userinfo in');
-  res.render('userinfo');
+  if (req.session.uid) {
+   user_sql((ck) => {
+     res.redirect('/userinfo', {ck:ck})
+   })
+  }
 });
 
 module.exports = router;
